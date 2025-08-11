51e587f1fffd0bcc103f578b2f5dd89a547453a4
vivekananda-pamidi <vivekpamidi9666@gmail.com>
2025-08-11 16:08:43 +0530

    Added Otp file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './page/Home'
 import Login from './page/Login'
 import Registration from './page/Registration'
+import Otp from './page/Otp'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
+        <Route path="/otp" element={<Otp />} />
       </Routes>
     </div>
   )
